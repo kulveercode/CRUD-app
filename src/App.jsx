e2732715dfc116c7b3086d8844885c5d6f9ccc4c
Kulveer Singh <kulveer.coder@gmail.com>
2023-11-24 11:02:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Create from './Create'
+
+export default function App() {
+  return (
+    <div>
+      <Create />
+    </div>
+  )
+}
---
+++ src/Create.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+function Create() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const header = { "Access-Control-Allow-Origin": "*"};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("https://655f7193879575426b455b36.mockapi.io/crud",
+    {
+      name: name,
+      email: email,
+      header
+    });
+  }
+
+  return (
+    <>
+      <h1>Create</h1>
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        {/* <div className="mb-3 form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label">Check me out</label>
+  </div> */}
+        <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default Create;
